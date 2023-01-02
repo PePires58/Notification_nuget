fbb3faf7167d26ffd7e49f7bf87c899f36a705a4
Pedro Henrique Pires <pedrohenriquepires13@gmail.com>
2023-01-02 11:16:55 -0300

    feat: adding notification service

---
+++ src/Notification.Application/Validators/NotificationValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Notification.Application.Validators
+{
+    [ExcludeFromCodeCoverage]
+    public class NotificationValidator : AbstractValidator<Domain.Entities.Notification>
+    {
+        public NotificationValidator()
+        {
+            RuleFor(c => c)
+                .Custom((c, context) =>
+                {
+                    EnsureInstanceNotNull(c);
+                });
+
+            RuleFor(c => c.Message)
+                .NotNull().WithMessage("notification message cannot be empty or null")
+                .NotEmpty().WithMessage("notification message cannot be empty or null");
+        }
+    }
+}
---
+++ src/Notification.Tests/ValidationService/ValidationServiceCheckIfObjectIsValidTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Notification.Application.Services;
+using Notification.Application.Validators;
+using Notification.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notification.Tests.ValidationService
+{
+    [TestClass]
+    public class ValidationServiceCheckIfObjectIsValidTests
+    {
+        #region Properties
+        public INotificatorService NotificatorService { get; set; }
+        public IValidationService ValidationService { get; set; }
+        #endregion
+
+        #region Constructor
+        public ValidationServiceCheckIfObjectIsValidTests()
+        {
+            NotificatorService = new NotificatorService();
+            ValidationService = new Application.Services.ValidationService(NotificatorService);
+        }
+        #endregion
+
+        #region Methods
+        
+        [TestMethod]
+        public void CheckIfObjectIsValidShouldReturnFalse()
+        {
+            Domain.Entities.Notification notification = new()
+            {
+                Message = string.Empty
+            };
+
+            bool objectIsValid = ValidationService.CheckIfObjectIsValid(new NotificationValidator(), notification);
+
+            Assert.IsFalse(objectIsValid);
+            Assert.IsTrue(NotificatorService.HasNotification);
+            Assert.IsTrue(NotificatorService.Any(m => m.Message == "notification message cannot be empty or null"));
+        }
+
+        [TestMethod]
+        public void CheckIfObjectIsValidShouldReturnTrue()
+        {
+            Domain.Entities.Notification notification = new()
+            {
+                Message = "notification"
+            };
+
+            bool objectIsValid = ValidationService.CheckIfObjectIsValid(new NotificationValidator(), notification);
+
+            Assert.IsTrue(objectIsValid);
+            Assert.IsFalse(NotificatorService.HasNotification);
+        }
+        #endregion
+    }
+}
